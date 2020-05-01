564eb8b52cd30e047753c8270273cb2954893bbe
Яцков Владислав Николаевич <56602271+Vladislavww@users.noreply.github.com>
2020-05-01 21:48:16 +0300

    Added entire project

---
+++ semestr_task_client/src/bsu/rfe_g6k2/Yackou/client/Main.java
@@ -0,0 +1,19 @@
+package bsu.rfe_g6k2.Yackou.client;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+public class Main {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable(){
+			public void run() {
+				final LoginFrame login_frame = new LoginFrame();
+				login_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				login_frame.setVisible(true);
+			}
+		});
+
+	}
+
+}
---
+++ semestr_task_client/src/bsu/rfe_g6k2/Yackou/client/NetClass.java
@@ -0,0 +1,156 @@
+package bsu.rfe_g6k2.Yackou.client;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+public class NetClass{
+	private static final int CLIENT_PORT = 1560;
+	private static final int SERVER_PORT = 4512;
+	private static final String SERVER_ADDRESS = "127.0.0.1";
+	private ArrayList<MessageListener> listeners = new ArrayList<MessageListener>(5);
+	public NetClass(){
+		new Thread(new Runnable() {
+			public void run(){
+				try{
+					final ServerSocket serverSocket = new ServerSocket(CLIENT_PORT);
+					while (!Thread.interrupted()){
+						//принятие ообщений от сервера
+						final Socket socket = serverSocket.accept();
+						final DataInputStream in = new DataInputStream(socket.getInputStream());
+						String work_type = in.readUTF();
+						if(work_type.equals("CHECK_IN")){//результат авторизации
+							String answer = in.readUTF();
+							notifyListeners(answer);
+						}
+						else if(work_type.equals("NEW_USER")){//результат создания нового пользователя
+							String answer = in.readUTF();
+							notifyListeners(answer);
+						}
+						//получение фотографий с сервера
+						else if(work_type.equals("NEXT_PHOTO")||work_type.equals("PREV_PHOTO")){
+							int bytesSize = in.readInt();
+							byte[] bytes = new byte[bytesSize];
+							for(int i=0; i<bytesSize; i++){
+								bytes[i] = in.readByte();
+							}
+							notifyListeners(bytes, bytesSize);
+						}
+						socket.close();
+					}
+					
+				} 
+				catch (IOException e) {
+				}
+			}
+		}).start();
+	}
+	
+	
+	//функция для отправки логина и пароля и команд
+	public int send(String worktype, String login, String password){
+		try{
+			final Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+			final DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			out.writeUTF(worktype);
+			out.writeUTF(login);
+			out.writeUTF(password);
+			if(worktype.equals("CHECK_IN")){
+				Integer port = CLIENT_PORT;
+				out.writeUTF(port.toString());
+			}
+			socket.close();
+			return 0;
+		}
+		catch (UnknownHostException e){
+			e.printStackTrace();
+			return 1;
+		} 
+		catch (IOException e){
+			e.printStackTrace();
+			return 2;
+		}
+	}
+	
+	//функция для отправки фотографий
+	public int send(String worktype, String name, String password, byte[] bytesFigure, int bytesFigureSize){
+		try{
+			final Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+			final DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			out.writeUTF(worktype);
+			out.writeUTF(name);
+			out.writeUTF(password);
+			out.writeInt(bytesFigureSize);
+			for(int i=0; i<bytesFigureSize; i++){
+				out.writeByte(bytesFigure[i]);
+			}
+			socket.close();
+			return 0;
+		}
+		catch (UnknownHostException e){
+			e.printStackTrace();
+			return 1;
+		} 
+		catch (IOException e){
+			e.printStackTrace();
+			return 2;
+		}
+	}
+	
+	//для отправки запроса на просмотр следующей/предыдущей фотографии
+	//или удаления фотографии
+	/*public int send(String worktype, String name){
+		try{
+			final Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+			final DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			out.writeUTF(worktype);
+			out.writeUTF(name);
+			socket.close();
+			return 0;
+		}
+		catch (UnknownHostException e){
+			e.printStackTrace();
+			return 1;
+		} 
+		catch (IOException e){
+			e.printStackTrace();
+			return 2;
+		}
+	}*/
+	
+	public void addMessageListener(MessageListener listener) {
+		synchronized (listeners){
+			listeners.add(listener);
+		}
+	}
+	
+	public void removeMessageListener(MessageListener listener) {
+		synchronized (listeners) {
+			listeners.remove(listener);
+		}
+	}
+	
+	private void notifyListeners(String message) {
+		synchronized (listeners){
+			int size = listeners.size();
+			for(int i=0; i<size; i++){
+				listeners.get(i).messageReceived(message);
+			}
+		}
+	}
+	
+	private void notifyListeners(byte[] bytes, int bytesSize) {
+		synchronized (listeners){
+			int size = listeners.size();
+			for(int i=0; i<size; i++){
+				listeners.get(i).messageReceived(bytes, bytesSize);
+			}
+		}
+	}
+}

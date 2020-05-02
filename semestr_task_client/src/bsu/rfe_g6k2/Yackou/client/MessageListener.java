3636f2dfd6ab1a44b40afa7ac2a0ddb0a0c20e3d
Яцков Владислав Николаевич <56602271+Vladislavww@users.noreply.github.com>
2020-05-02 09:30:07 +0300

    Delete MessageListener.java

--- semestr_task_client/src/bsu/rfe_g6k2/Yackou/client/MessageListener.java
+++
@@ -1,8 +0,0 @@
-package bsu.rfe_g6k2.Yackou.client;
-
-//интерфейс, чтобы сделать слушатель получения сообщений
-public interface MessageListener{
-	void messageReceived(String message);
-	void messageReceived(String name, String message);
-	void messageReceived(byte[] bytes, int bytesSize);
-}
